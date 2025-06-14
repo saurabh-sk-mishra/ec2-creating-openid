a8d2ab4f3b881560a4acf6e1a36e102ed91c5a27
saurabh-sk-mishra <skvedicestates@gmail.com>
2025-06-14 15:57:44 +0530

    Update ec2-creation.tf

--- ec2-creation.tf
+++ ec2-creation.tf
@@ -1,4 +1,4 @@
-name: Launch EC2 Instance
+name: Auto EC2 Launch
 
 on:
   push:
@@ -7,33 +7,15 @@ on:
   pull_request:
     branches:
       - main
-  workflow_dispatch:  # still allows manual runs
+  workflow_dispatch:  # optional manual trigger
 
 permissions:
   id-token: write
   contents: read
 
 jobs:
-  launch-ec2:
+  test-run:
     runs-on: ubuntu-latest
-
     steps:
-    - name: Checkout repo
-      uses: actions/checkout@v3
-
-    - name: Configure AWS credentials via OIDC
-      uses: aws-actions/configure-aws-credentials@v2
-      with:
-        role-to-assume: arn:aws:iam::721689331129:role/github-action
-        role-session-name: github-action
-        aws-region: us-east-1
-
-    - name: Launch EC2 instance
-      run: |
-        echo "Launching EC2..."
-        aws ec2 run-instances \
-          --image-id ami-0c94855ba95c71c99 \
-          --count 1 \
-          --instance-type t2.micro \
-          --key-name Jenkins \
-          --security-groups default
+      - name: Hello from GitHub Actions
+        run: echo "Hello, workflow ran!"
